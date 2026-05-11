19da8f34b31aa0fd7637642e14d766e2510f263a
lucas <luczssz3@gmail.com>
2026-05-11 11:30:04 -0300

    feat:fazendo os produtos no js

---
+++ src/js/script.js
@@ -0,0 +1,49 @@
+//array com os produtos
+
+const produtos = [
+    {
+        id:1,
+        emoji:"🏴‍☠️",
+        badge:"One Piece",
+        nome:"Monkey D. Luffy",
+        descricao:"Edição gear five com efeitos e base o NIKA ",
+        preco:349.99,
+
+    },
+    {
+        id:2,
+        emoji:"🦇",
+        badge:"DC Comics",
+        nome:"Batman Dark Knight",
+        descricao:"Edição premium com capa removivel e batarangues inclusos.",
+        preco:319.90
+
+    },
+    {
+        id:3,
+        emoji:"🟢",
+        badge:"Star Wars",
+        nome:"Mestre Yoda",
+        descricao:"Edição com sabre de luz removivel e base flutuante magnetica",
+        preco:289.90,
+
+    },
+    {
+        id:4,
+        emoji:"🎌",
+        badge:"JoJo's Bizarre",
+        nome:"Jotaro Kujo",
+        descricao:"Edição especial, Jotaro Star Platinum, bone removivel",
+        preco:449.99,
+
+    },
+    {
+        id:5,
+        emoji:"⚡",
+        badge:"Marvel",
+        nome:"Thor Ragnarok",
+        descricao:"Edição com martelo mjolnir e capa removivel ",
+        preco:279.90,
+
+    }
+];
